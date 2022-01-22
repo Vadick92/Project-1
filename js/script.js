016dc18a264ab689d66452abaeb11423d5242cdb
Vadim <vadick92@yandex.ru>
2022-01-22 13:51:23 +0500

    первый комит

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log('arr' + '-object');
+
+let incr = 10,
+    decr = 10;
+
+
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(2*4 === '8');
+    
+    let and = true;
+    let or = false;
+    console.log(and||or);
